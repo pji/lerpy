e24d5a29f081c391d9826b33807471e677c252e1
Paul J. Iutzi <pji@mac.com>
2021-09-10 07:27:19 -0500

    Started breakup of resize_array.

--- lerpy/lerpy.py
+++ lerpy/lerpy.py
@@ -7,6 +7,10 @@ Linear interpolation functions.
 import numpy as np
 
 
+# Constants.
+X, Y = -1, -2
+
+
 # Public functions.
 def linear_interpolation(a: np.ndarray,
                          b: np.ndarray,
@@ -31,6 +35,114 @@ def linear_interpolation(a: np.ndarray,
         array([2., 3., 4.])
     """
     return a * (1 - x) + b * x
+
+
+def resize_array(a: np.ndarray, size: tuple[int, ...]) -> np.ndarray:
+    """Resize an two dimensional array using linear interpolation.
+
+    :param a: The array to resize. The array is expected to have at
+        least two dimensions.
+    :param size: The shape for the resized array.
+    :return: A :class:ndarray object.
+    :rtype: numpy.ndarray
+    """
+    # Perform defensive actions to prevent unneeded processing if
+    # the array won't actually change and to make sure any changes
+    # to the array won't have unexpected side effects.
+    if size == a.shape:
+        return a
+    a = a.copy()
+
+    # Map out the relationship between the old space and the
+    # new space.
+    whole, parts = _map_resized_array(a, size)
+    grids = _build_grids(a, size, whole)
+
+    # Perform the interpolation using the mapped space and return.
+    return _n_dimensional_interpolation(grids, parts)
+
+
+# Private functions.
+def _build_grids(a: np.ndarray,
+                 size: tuple[int, ...],
+                 whole: np.ndarray) -> dict[str, np.ndarray]:
+    """Build the grids used in an n-dimensional interpolation."""
+    # Linear interpolation determines the value of a new pixel by
+    # comparing the values of the eight old pixels that surround it.
+    # The hashes are the keys to the dictionary that contains those
+    # old pixel values. The key indicates the position of the pixel
+    # on each axis, with one meaning the position is ahead of the
+    # new pixel, and zero meaning the position is behind it.
+    hashes = [f'{n:>02b}'[::-1] for n in range(2 ** len(size))]
+    hash_table = {}
+
+    # The original array needs to be made one dimensional for the
+    # numpy.take operation that will occur as we build the tables.
+    raveled = np.ravel(a)
+
+    # Build the table that contains the old pixel values to
+    # interpolate.
+    for hash in hashes:
+        hash_whole = whole.copy()
+
+        # Use the hash key to adjust the which old pixel we are
+        # looking at.
+        for axis in Y, X:
+            if hash[axis] == '1':
+                hash_whole[axis] += 1
+
+                # Handle the pixels that were pushed off the far
+                # edge of the original array by giving them the
+                # value of the last pixel along that axis in the
+                # original array.
+                m = np.zeros(hash_whole[axis].shape, dtype=bool)
+                m[hash_whole[axis] >= a.shape[axis]] = True
+                hash_whole[axis][m] = a.shape[axis] - 1
+
+        # Since numpy.take() only works in one dimension, we need to
+        # map the three dimensional indices of the original array to
+        # the one dimensional indices used by the raveled version of
+        # that array.
+        raveled_indices = hash_whole[Y] * a.shape[X]
+        raveled_indices += hash_whole[X]
+
+        # Get the value of the pixel in the original array.
+        hash_table[hash] = np.take(raveled, raveled_indices.astype(int))
+
+    return hash_table
+
+
+def _map_resized_array(a: np.ndarray,
+                       size: tuple[int, ...]) -> tuple[np.ndarray, np.ndarray]:
+    """Map out how the values of a grid are spread when the grid
+    is resized.
+    """
+    indices = np.indices(size)
+    new_ends = [s - 1 for s in size]
+    old_ends = [s - 1 for s in a.shape]
+    true_factors = [n / o for n, o in zip(new_ends, old_ends)]
+    whole = indices.copy()
+    parts = indices.copy().astype(float)
+    for i in Y, X:
+        whole[i] = (indices[i] // true_factors[i]).astype(int)
+        parts[i] = (indices[i] / true_factors[i] - whole[i]).astype(float)
+    return whole, parts
+
+
+def _n_dimensional_interpolation(grids: dict[str, np.ndarray],
+                                 parts: np.ndarray) -> np.ndarray:
+    """Interpolate the values of each pixel of image data."""
+    if len(grids) > 2:
+        new_grids = {}
+        evens = [k for k in grids if k.endswith('0')]
+        odds = [k for k in grids if k.endswith('1')]
+        for even, odd in zip(evens, odds):
+            new_key = even[:-1]
+            axis = len(new_key)
+            new_grids[new_key] = lerp(grids[even], grids[odd], parts[-1])
+        return _n_dimensional_interpolation(new_grids, parts[:-1])
+
+    return lerp(grids['0'], grids['1'], parts[-1])
 
 
 # Function aliases.
--- tests/test_lerpy.py
+++ tests/test_lerpy.py
@@ -2,6 +2,8 @@
 test_lerpy
 ~~~~~~~~~~
 """
+import unittest as ut
+
 import numpy as np
 
 from lerpy import lerpy as lp
@@ -9,6 +11,7 @@ from tests.common import ArrayTestCase
 
 
 # Test cases.
+@ut.skip
 class BilinearInterpolationTestCase(ArrayTestCase):
     def test_bilinear_interpolation(self):
         """Given two two-dimensional arrays of values, and an array
@@ -23,7 +26,7 @@ class BilinearInterpolationTestCase(ArrayTestCase):
             [1.50, 1.75, 2.00, 2.25, 2.50, ],
             [2.00, 2.25, 2.50, 2.75, 3.00, ],
         ])
-        
+
         # Test data and setup.
         a = np.array([
             [0.00, 0.00, 0.00, 0.00, 1.00, ],
@@ -41,24 +44,24 @@ class BilinearInterpolationTestCase(ArrayTestCase):
         ])
         x = np.array([
             [
-                [0.00, 0.25, 0.50, 0.75, 1.00, ],
-                [0.00, 0.25, 0.50, 0.75, 1.00, ],
-                [0.00, 0.25, 0.50, 0.75, 1.00, ],
-                [0.00, 0.25, 0.50, 0.75, 1.00, ],
-                [0.00, 0.25, 0.50, 0.75, 1.00, ],
-            ],
-            [
                 [0.00, 0.00, 0.00, 0.00, 0.00, ],
                 [0.25, 0.25, 0.25, 0.25, 0.25, ],
                 [0.50, 0.50, 0.50, 0.50, 0.50, ],
                 [0.75, 0.75, 0.75, 0.75, 0.75, ],
                 [1.00, 1.00, 1.00, 1.00, 1.00, ],
             ],
+            [
+                [0.00, 0.25, 0.50, 0.75, 1.00, ],
+                [0.00, 0.25, 0.50, 0.75, 1.00, ],
+                [0.00, 0.25, 0.50, 0.75, 1.00, ],
+                [0.00, 0.25, 0.50, 0.75, 1.00, ],
+                [0.00, 0.25, 0.50, 0.75, 1.00, ],
+            ],
         ])
-        
+
         # Run test.
         act = lp.bilinear_interpolation(a, b, x)
-        
+
         # Determine test result.
         self.assertArrayEqual(exp, act)
 
@@ -72,13 +75,43 @@ class LinearInterpolationTestCase(ArrayTestCase):
         # Expected value.
         exp = np.array([0.5, 1.5, 2.5, 3.5, 4.5, ])
 
-        # Test data and setup.
+        # Test data and state.
         a = np.array([0.0, 1.0, 2.0, 3.0, 4.0, ])
         b = np.array([1.0, 2.0, 3.0, 4.0, 5.0, ])
         x = np.array([0.5, 0.5, 0.5, 0.5, 0.5, ])
 
         # Run test.
         act = lp.linear_interpolation(a, b, x)
+
+        # Determine test result.
+        self.assertArrayEqual(exp, act)
+
+
+class ResizeArrayTestCase(ArrayTestCase):
+    def test_resize_two_dimensions(self):
+        """Given a two-dimensional array and a new size, return an
+        array of the new size, with the data resized through bilinear
+        interpolation.
+        """
+        # Expected values.
+        exp = np.array([
+            [0.0, 0.5, 1.0, 1.5, 2.0, ],
+            [0.5, 1.0, 1.5, 2.0, 2.5, ],
+            [1.0, 1.5, 2.0, 2.5, 3.0, ],
+            [1.5, 2.0, 2.5, 3.0, 3.5, ],
+            [2.0, 2.5, 3.0, 3.5, 4.0, ],
+        ])
+
+        # Test data and state.
+        a = np.array([
+            [0.0, 1.0, 2.0, ],
+            [1.0, 2.0, 3.0, ],
+            [2.0, 3.0, 4.0, ],
+        ])
+        size = (5, 5)
+
+        # Run test.
+        act = lp.resize_array(a, size)
 
         # Determine test result.
         self.assertArrayEqual(exp, act)
